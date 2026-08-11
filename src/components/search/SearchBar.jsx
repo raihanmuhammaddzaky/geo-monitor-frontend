444c23470c2038d96ae6146add532dbf5b696303
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2026-08-11 12:05:03 +0700

    update

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function SearchBar({ isExpanded, setIsExpanded }) {
+    const { searchQueary, setSearchQuery } = useMap();
     return (
         <div className="px-5 pt-6 pb-4 border-b border-border-subtle bg-white z-20 flex gap-3 items-center">
             <label className="flex flex-col flex-1 h-11 min-w-0">
@@ -8,20 +9,21 @@ export default function SearchBar({ isExpanded, setIsExpanded }) {
                     <div className="text-muted flex items-center justify-center pl-3 rounded-l-xl">
                         <span className="material-symbols-outlined text-[20px]">search</span>
                     </div>
-                    <input 
-                        className="flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-r-xl text-text-main focus:outline-none focus:ring-0 border-none bg-transparent h-full placeholder:text-muted px-3 text-sm font-medium" 
-                        placeholder="Search locations, IDs..." 
+                    <input
+                        className="flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-r-xl text-text-main focus:outline-none focus:ring-0 border-none bg-transparent h-full placeholder:text-muted px-3 text-sm font-medium"
+                        placeholder="Search locations, IDs..."
                         type="text"
+                        value={searchQueary}
+                        onChange={(e) => { setSearchQuery(e.target.value) }}
                     />
                 </div>
             </label>
-            <button 
+            <button
                 onClick={() => setIsExpanded(!isExpanded)}
-                className={`flex h-11 w-11 shrink-0 items-center justify-center rounded-xl transition-colors border shadow-sm ${
-                    isExpanded 
-                    ? 'bg-primary/10 text-primary border-primary/30' 
-                    : 'bg-white text-muted hover:bg-gray-50 border-border-subtle'
-                }`}
+                className={`flex h-11 w-11 shrink-0 items-center justify-center rounded-xl transition-colors border shadow-sm ${isExpanded
+                        ? 'bg-primary/10 text-primary border-primary/30'
+                        : 'bg-white text-muted hover:bg-gray-50 border-border-subtle'
+                    }`}
                 title="Toggle Filters"
             >
                 <span className="material-symbols-outlined text-[22px]">
